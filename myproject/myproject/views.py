1ad3cfe59c3d1050dc8481c7f2066a128993b6f7
mehmetsan <trabzonpower@gmail.com>
2021-01-30 19:05:34 +0300

    Thread implemented

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -4,6 +4,10 @@ from bs4 import BeautifulSoup
 from xml.sax.handler import ContentHandler
 from xml.sax import make_parser
 from entries.models import Entry
+import threading
+
+
+from django.core.mail import send_mail
 
 def check_syntax(link):
     try:
@@ -16,30 +20,49 @@ def check_syntax(link):
         print ("{} is NOT well-formed! {}".format("Your doc is ", e))
         return e
 
-def home(request):
+
+def processUrl(request):
     content = ""
+    link = request.POST['link-url']
+    user_email = request.POST['email']
+    errors = check_syntax(link)
 
-    if request.method == "POST":
-        link = request.POST['link-url']
-        errors = check_syntax(link)
-
-        if errors:
-            ## SEND EMAIL
-            pass
-        else:
-            # SAVE THE FILE
-            req = urlopen(link)
-            soup = BeautifulSoup(req, 'lxml')
-
-            content = soup.prettify()
-       
-            f = open("sample.xml", "w")
-            f.write(content)
-            f.close()
-
-            Entry.objects.create(user=request.user, entry_xml=content, entry_link=link)
+    if errors:
+        ## SEND EMAIL
+        send_mail(
+            "Your XML Input Failed",
+            str(errors),
+            "mehmettest@gmail.com",
+            [user_email],
+        )
+        return False
         
+    else:
+        # SAVE THE FILE
+        req = urlopen(link)
+        soup = BeautifulSoup(req, 'lxml')
 
-        return render( request, 'home.html', {'data':content})
+        content = soup.prettify()
+    
+        f = open("sample.xml", "w")
+        f.write(content)
+        f.close()
+
+        Entry.objects.create(user=request.user, entry_xml=content, entry_link=link)
+        return True 
+
+
+
+def home(request):
+    
+    if request.method == "POST":
+        t = threading.Thread(target=processUrl,
+                            args=(request,)
+                            )
+        t.setDaemon(True)
+        t.start()
+
+
+        return render( request, 'home.html', {})
     return render( request, 'home.html', {})
     
